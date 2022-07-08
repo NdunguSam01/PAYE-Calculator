c2c4407cb7065ef1d5aad3c2a259c109a27f08c6
NdunguSam01 <ndungu.muigai01@gmail.com>
2022-07-08 13:39:12 +0300

    Few implementations working

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,4 +1,224 @@
-const nsold =()=> nscont=200;
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+let salary=document.getElementById('salary');
+let bonus=document.getElementById('bonus');
+
+let nssfDeduction=document.getElementById("nssfDeduct");
+const calculate=()=>
+{
+    document.getElementById('initial').textContent='Kshs ' +parseInt(salary.value).toFixed(2);
+    document.getElementById('benefits').textContent='Kshs ' +parseInt(bonus.value).toFixed(2);
+    nhif()
+    nssf()
+    relief()
+    incomeAfterDeductions()
+    tax()
+}
+
+//NSSF calculations
+const nssf=()=>
+{
+    let nsyes=document.getElementById('nssfyes');
+    let nsno=document.getElementById('nssfno');
+    let rateNew=document.getElementById('nsnew');
+    let rateOld=document.getElementById('nsold');
+
+    if (nsyes.checked)
+    {
+        if(rateNew.checked)
+        {
+            if(salary.value>18000)
+            {
+                document.getElementById('nssfDeduct').textContent='Kshs 2160';
+            }
+            else
+            {
+                let nssf=parseInt(salary.value *0.12);
+                document.getElementById('nssfDeduct').textContent='Kshs ' + nssf.toFixed(2);
+            }
+        }
+        if(rateOld.checked)
+        {
+            document.getElementById('nssfDeduct').textContent='Kshs 200';
+        }
+    }
+    else if(nsno.checked)
+    {
+        document.getElementById('nssfDeduct').textContent='Kshs 0.00';
+    }
+}
+
+//NHIF calculations
+const nhif=()=>
+{
+    let nhyes=document.getElementById('nhyes');
+    let nhno=document.getElementById('nhno');
+
+    if(nhyes.checked)
+    {
+        if (salary.value>= 1000 && salary.value<= 5999) 
+        {
+            nhcont= 150;
+        } 
+        else if (salary.value>= 6000 && salary.value<= 7999) 
+        {
+            nhcont= 300;
+        } 
+        else if (salary.value>= 8000 && salary.value<= 11999) 
+        {
+            nhcont= 400;
+        } 
+        else if (salary.value>= 12000 && salary.value<= 14999) 
+        {
+            nhcont= 500;
+        } 
+        else if (salary.value>= 15000 && salary.value<= 19999) 
+        {
+            nhcont= 600;
+        } 
+        else if (salary.value>= 20000 && salary.value<= 24999) 
+        {
+            nhcont= 750;
+        } 
+        else if (salary.value>= 25000 && salary.value<= 29999) 
+        {
+            nhcont= 850;
+        } 
+        else if (salary.value>= 30000 && salary.value<= 34999) 
+        {
+            nhcont= 900;
+        } 
+        else if (salary.value>= 35000 && salary.value<= 39999) 
+        {
+            nhcont= 950;
+        } 
+        else if (salary.value>= 40000 && salary.value<= 44999) 
+        {
+            nhcont= 1000;
+        } 
+        else if (salary.value>= 45000 && salary.value<= 49999) 
+        {
+            nhcont= 1100;
+        }
+        else if (salary.value>= 50000 && salary.value<= 59999)
+        {
+            nhcont= 1200;
+        } 
+        else if (salary.value>= 60000 && salary.value<= 69999) 
+        {
+            nhcont= 1300;
+        } 
+        else if (salary.value>= 70000 && salary.value<= 79999) 
+        {
+            nhcont= 1400;
+        } 
+        else if (salary.value>= 80000 && salary.value<= 89999) 
+        {
+            nhcont= 1500;
+        } 
+        else if (salary.value>= 90000 && salary.value<= 99999) {
+            nhcont= 1600;
+        } 
+        else if (salary.value>= 100000) 
+        {
+            nhcont= 1700;
+        }
+        else 
+        {
+            document.getElementById("nhifcontrib").textContent = 'Kshs 0.00';
+        }
+   
+        document.getElementById("nhifcontrib").textContent = 'Kshs ' +nhcont;
+    }
+    else if (nhno.checked)
+    {
+        document.getElementById("nhifcontrib").textContent = 'Kshs 0.00';
+    } 
+}
+
+//Relief Amount
+const relief=()=>
+{
+    if (month.checked)
+    {
+        document.getElementById("relief").textContent = 'Kshs 2400';
+    }
+    else if (year.checked)
+    {
+        document.getElementById("relief").textContent = 'Kshs 28800';
+    }
+}
+
+//Income After Pension Deductions
+const incomeAfterDeductions =()=>
+{
+    let netamount =parseInt(salary.value-nssfDeduction.textContent);
+    document.getElementById("afterDeduct").textContent = 'Kshs ' +netamount.toFixed(2);
+    document.getElementById("taxable").textContent = 'Kshs ' +netamount.toFixed(2);
+    document.getElementById("charged").textContent = 'Kshs ' +netamount.toFixed(2);
+}
+
+//PAYE calculations
+const tax=()=>
+{
+    amount=document.getElementById("taxable").value;
+    if(monthly.checked)
+    {
+        if(amount>=0 && amount<=12298)
+        {
+            taxAmnt=amount*0.1;
+        }
+        else if(amount>=12299 && amount<=23885)
+        {
+            taxAmnt=amount*0.15;
+        }
+        else if(amount>=23886 && amount<=35472)
+        {
+            taxAmnt=amount*0.2;
+        }
+        else if(amount>=35473 && amount<=47059)
+        {
+            taxAmnt=amount*0.25;
+        } 
+        else if  (amount>47059)
+        {
+            taxAmnt=amount*0.3;
+        }
+        else
+        {
+            document.getElementById("tax").textContent = 'Tax bracket not found!';
+        }
+    document.getElementById("tax").textContent = 'Kshs ' +taxAmnt;
+    }
+    else if(year.checked)
+    {
+        if  (amount>=0 && amount<=147580)
+        {
+            taxAmnt=amount*0.1;
+        }
+        else if (amount>=147581 && amount<=286623)
+        {
+            taxAmnt=amount*0.15;
+        }
+        else if(amount>=286624 && amount<=425666)
+        {
+            taxAmnt=amount*0.2;
+        }
+        else if(amount>=425667 && amount<=564709)
+        {
+            taxAmnt=amount*0.25;
+        }
+        else if(amount>564709)
+        {
+            taxAmnt=amount*0.3;
+        }
+        else
+        {
+            document.getElementById("tax").textContent = 'Tax bracket not found!';
+        }
+    }
+        document.getElementById("tax").textContent = 'Kshs ' +taxAmnt;
+}
 
 const monthly=(amount)=>
 {
@@ -60,81 +280,5 @@ const annually=(amount)=>
 }
 
 
-const nhif=(salary)=>
-{
-    switch(salary) 
-    {
-        case salary>=0 && salary<=5999:
-            nhcont=150;
-            break;
-        
-        case salary>=6000 && salary<=7999:
-            nhcont=300;
-            break;
 
-        case salary>=8000 && salary<=11999:
-            nhcont=400;
-            break;
-        
-        case salary>=12000 && salary<=14999:
-            nhcont=500;
-            break;
 
-        case salary>=15000 && salary<=19999:
-            nhcont=600;
-            break;
-
-        case salary>=20000 && salary<=24999:
-            nhcont=750;
-            break;
-
-        case salary>=25000 && salary<=29999:
-            nhcont=850;
-            break;
-        
-        case salary>=30000 && salary<=34999:
-            nhcont=900;
-            break;
-
-        case salary>=35000 && salary<=39999:
-            nhcont=950;
-            break;
-
-        case salary<=40000 && salary<=44999:
-            nhcont=1000;
-            break;
-
-        case salary>=45000 && salary<=49999:
-            nhcont=1100;
-            break;
-
-        case salary<=50000 && salary<=59999:
-            nhcont=1200;
-            break;
-
-        case salary<=60000 && salary<=69999:
-            nhcont=1300;
-            break;
-
-        case salary<=70000 && salary<=79999:
-            nhcont=1400;
-            break;
-
-        case salary<=80000 && salary<=89999:
-            nhcont=1500;
-            break;
-
-        case salary<=90000 && salary<=99999:
-            nhcont=1600;
-            break;
-
-        case salary>=100000:
-            nhcont=1700;
-            break;
-
-        default:
-            alert ("Bracket not found!");
-
-    }
-    return nhcont;
-}
